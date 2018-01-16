b9ed7f2ca6cb8c41f3d58718c8a3086932047474
Andris Reinman <andris@kreata.ee>
2018-01-16 15:45:50 +0200

    Allow some changes on mailboxes

--- routes/webmail.js
+++ routes/webmail.js
@@ -125,9 +125,11 @@ router.get('/:mailbox/message/:message', (req, res, next) => {
 
             messageData.info = info;
             res.render('webmail/message', {
+                layout: 'layout-webmail',
                 activeWebmail: true,
                 mailboxes,
                 mailbox: selectedMailbox,
+
                 message: messageData,
                 messageJson: JSON.stringify(messageData).replace(/\//g, '\\u002f')
             });
@@ -297,6 +299,11 @@ router.get('/:mailbox/audit/:message', (req, res, next) => {
                 }
 
                 res.render('webmail/audit', {
+                    layout: 'layout-webmail',
+                    activeWebmail: true,
+                    mailboxes,
+                    mailbox: selectedMailbox,
+
                     events: events.map(event => {
                         switch (event.action) {
                             case 'STORE':
@@ -357,15 +364,217 @@ router.get('/:mailbox/audit/:message', (req, res, next) => {
 
                         return event;
                     }),
-                    activeWebmail: true,
-                    mailboxes,
                     messageData,
-                    mailbox: selectedMailbox,
                     message: result.value.message,
                     forwardTargets
                 });
             });
         });
+    });
+});
+
+router.get('/:mailbox/settings', (req, res, next) => {
+    const schema = Joi.object().keys({
+        mailbox: Joi.string()
+            .hex()
+            .length(24)
+    });
+
+    let result = Joi.validate(req.params, schema, {
+        abortEarly: false,
+        convert: true,
+        allowUnknown: true
+    });
+
+    if (result.error) {
+        if (result.error && result.error.details) {
+            result.error.details.forEach(detail => {
+                req.flash('danger', detail.message);
+            });
+        }
+        return res.redirect('/webmail');
+    }
+
+    apiClient.mailboxes.list(req.user.id, true, (err, mailboxes) => {
+        if (err) {
+            return next(err);
+        }
+
+        let parents = new Map();
+
+        let mailbox = result.value.mailbox || mailboxes[0].id;
+        let mailboxExists = false;
+        let selectedMailbox = false;
+        let parentPath = false;
+
+        mailboxes.forEach((entry, i) => {
+            entry.index = i + 1;
+
+            let parts = entry.path.split('/');
+            console.log(parts);
+            console.log(parts.slice(0, 1));
+
+            if (entry.id === mailbox) {
+                entry.selected = true;
+                mailboxExists = true;
+                selectedMailbox = entry;
+            }
+
+            for (let i = 0; i < parts.length; i++) {
+                let path = parts.slice(0, i + 1).join('/');
+                let mbox = {
+                    id: path === entry.path ? entry.id : false,
+                    path,
+                    level: i + 1,
+                    name: parts.slice(0, i + 1).join(' / '),
+                    prefix: '&nbsp;&nbsp;&nbsp;&nbsp'.repeat(i)
+                };
+                if (entry.path === path && entry.id === mailbox) {
+                    continue;
+                }
+                if (!parents.has(path) || path === entry.path) {
+                    parents.set(path, mbox);
+                }
+            }
+        });
+
+        if (selectedMailbox) {
+            parentPath = selectedMailbox.path.split('/');
+            parentPath.pop();
+            parentPath = parentPath.join('/');
+        }
+
+        parents = Array.from(parents).map(entry => {
+            let parent = entry[1];
+            if (parent.path === parentPath) {
+                parent.isParent = true;
+            }
+            return parent;
+        });
+
+        if (!mailboxExists) {
+            return res.redirect('/webmail');
+        }
+
+        console.log(require('util').inspect(mailboxes, false, 22));
+        console.log(parents);
+
+        res.render('webmail/mailbox', {
+            layout: 'layout-webmail',
+            activeWebmail: true,
+            mailboxes,
+            mailbox: selectedMailbox,
+
+            values: {
+                name: selectedMailbox.name
+            },
+
+            parents,
+
+            isSpecial: selectedMailbox.path === 'INBOX' || selectedMailbox.specialUse,
+
+            isInbox: selectedMailbox.path === 'INBOX',
+            isTrash: selectedMailbox.specialUse === '\\Trash',
+            isSent: selectedMailbox.specialUse === '\\Sent',
+            isJunk: selectedMailbox.specialUse === '\\Junk',
+
+            csrfToken: req.csrfToken()
+        });
+    });
+});
+
+router.post('/:mailbox/settings', (req, res) => {
+    const schema = Joi.object().keys({
+        mailbox: Joi.string()
+            .hex()
+            .length(24),
+        parent: Joi.string()
+            .default('')
+            .allow(''),
+        name: Joi.string()
+            .regex(/\//, { name: 'folder', invert: true })
+            .required()
+    });
+
+    req.body.mailbox = req.params.mailbox;
+    delete req.body._csrf;
+
+    let result = Joi.validate(req.body, schema, {
+        abortEarly: false,
+        convert: true,
+        allowUnknown: true
+    });
+
+    if (result.error) {
+        if (result.error && result.error.details) {
+            result.error.details.forEach(detail => {
+                req.flash('danger', detail.message);
+            });
+        }
+        return res.redirect('/webmail/' + result.value.mailbox + '/settings');
+    }
+
+    let path = result.value.parent
+        .split('/')
+        .concat(result.value.name.split('/') || [])
+        .map(name => name.trim())
+        .filter(name => name)
+        .join('/');
+
+    apiClient.mailboxes.update(
+        req.user.id,
+        result.value.mailbox,
+        {
+            path
+        },
+        (err, response) => {
+            console.log(err || response);
+
+            if (err) {
+                req.flash('danger', err.message);
+            }
+
+            if (response && response.success) {
+                req.flash('success', 'Mailbox folder was updated');
+            }
+
+            return res.redirect('/webmail/' + result.value.mailbox + '/settings');
+        }
+    );
+});
+
+router.post('/:mailbox/delete', (req, res) => {
+    const schema = Joi.object().keys({
+        mailbox: Joi.string()
+            .hex()
+            .length(24)
+    });
+
+    let result = Joi.validate(req.params, schema, {
+        abortEarly: false,
+        convert: true,
+        allowUnknown: true
+    });
+
+    if (result.error) {
+        if (result.error && result.error.details) {
+            result.error.details.forEach(detail => {
+                req.flash('danger', detail.message);
+            });
+        }
+        return res.redirect('/webmail');
+    }
+
+    apiClient.mailboxes.delete(req.user.id, result.value.mailbox, (err, result) => {
+        if (err) {
+            req.flash('danger', err.message);
+        }
+
+        if (result && result.success) {
+            req.flash('success', 'Mailbox folder was deleted');
+        }
+
+        return res.redirect('/webmail');
     });
 });
 
@@ -445,9 +654,11 @@ function renderMailbox(req, res, next) {
             }
 
             res.render('webmail/index', {
+                layout: 'layout-webmail',
                 activeWebmail: true,
                 mailboxes,
                 mailbox: selectedMailbox,
+
                 cursorType,
                 cursorValue,
                 page: result.page,
@@ -455,8 +666,12 @@ function renderMailbox(req, res, next) {
                 nextPage: result.page + 1,
                 previousCursor: result.previousCursor,
                 previousPage: Math.max(result.page - 1, 1),
+
+                isInbox: selectedMailbox.path === 'INBOX',
                 isTrash: selectedMailbox.specialUse === '\\Trash',
                 isSent: selectedMailbox.specialUse === '\\Sent',
+                isJunk: selectedMailbox.specialUse === '\\Junk',
+
                 messageRowTemplate: templates.messageRowTemplate,
                 messages: result.results.map(message => {
                     message.fromHtml = tools.getAddressesHTML(message.from, true);
