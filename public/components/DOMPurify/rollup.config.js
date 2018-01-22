2cfd63bc15a81645f3bfb1cfd665c435115444ba
Andris Reinman <andris@kreata.ee>
2018-01-22 20:22:06 +0200

    use bower

---
+++ public/components/DOMPurify/rollup.config.js
@@ -0,0 +1,39 @@
+const nodeResolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+const replace = require('rollup-plugin-replace');
+const uglify = require('rollup-plugin-uglify');
+
+const env = process.env.NODE_ENV;
+const isProd = env === 'production';
+const version = process.env.npm_package_version;
+
+const config = {
+  entry: 'src/purify.js',
+  external: [],
+  globals: {},
+  format: 'umd',
+  moduleName: 'DOMPurify',
+  sourceMap: true,
+  plugins: [
+    nodeResolve(),
+    babel({
+      exclude: '**/node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env),
+      VERSION: `'${version}'`,
+    }),
+  ],
+};
+
+if (isProd) {
+  config.plugins.push(
+    uglify({
+      compress: {
+        warnings: false,
+      },
+    })
+  );
+}
+
+module.exports = config;
